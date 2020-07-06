6b9d4db3b5d4615a5857748b5aa6e5ed2ff32dff
alexcront <crontalexandru@gmail.com>
2020-07-06 19:01:05 +0200

    Update the tools section and add the programming skills section

--- index.php
+++ index.php
@@ -71,6 +71,7 @@
           <li><a href="#skills"><i class="bx bx-server"></i> <span>Skills</span></a></li>
           <li><a href="#resume"><i class="bx bx-file-blank"></i> <span>Resume</span></a></li>
           <li><a href="#tools"><i class="bx bx-book-content"></i>Tools</a></li>
+          <li><a href="#programming_skills"><i class="bx bx-server"></i>Programming skills</a></li>
 <!--           <li><a href="#services"><i class="bx bx-server"></i> Services</a></li> -->
           <li><a href="#contact"><i class="bx bx-envelope"></i> Contact</a></li>
 
@@ -583,7 +584,7 @@ Engineering, representing the students in the Faculty Council and University Sen
 
         <div class="section-title">
           <h2>Tools</h2>
-          <p>Magnam dolores commodi suscipit. Necessitatibus eius consequatur ex aliquid fuga eum quidem. Sit sint consectetur velit. Quisquam quos quisquam cupiditate. Et nemo qui impedit suscipit alias ea. Quia fugiat sit in iste officiis commodi quidem hic quas.</p>
+          <p>Until now I used several tools in worder to have quality in my work.</p>
         </div>
 
         <div class="row">
@@ -652,6 +653,70 @@ Engineering, representing the students in the Faculty Council and University Sen
       </div>
     </section><!-- End Services Section -->
 
+    <!-- ======= Programming Skills Section ======= -->
+    <section id="programming_skills" class="skills section-bg">
+      <div class="container">
+
+        <div class="section-title">
+          <h2>Programming skills</h2>
+        </div>
+
+        <div class="row skills-content">
+
+          <div class="col-lg-6" data-aos="fade-up">
+
+            <div class="progress">
+              <span class="skill">C <i class="val">80%</i></span>
+              <div class="progress-bar-wrap">
+                <div class="progress-bar" role="progressbar" aria-valuenow="80" aria-valuemin="0" aria-valuemax="100"></div>
+              </div>
+            </div>
+
+            <div class="progress">
+              <span class="skill">C++ <i class="val">100%</i></span>
+              <div class="progress-bar-wrap">
+                <div class="progress-bar" role="progressbar" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100"></div>
+              </div>
+            </div>
+
+            <div class="progress">
+              <span class="skill">Python <i class="val">75%</i></span>
+              <div class="progress-bar-wrap">
+                <div class="progress-bar" role="progressbar" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100"></div>
+              </div>
+            </div>
+
+          </div>
+
+          <div class="col-lg-6" data-aos="fade-up" data-aos-delay="100">
+
+            <div class="progress">
+              <span class="skill">HTML <i class="val">80%</i></span>
+              <div class="progress-bar-wrap">
+                <div class="progress-bar" role="progressbar" aria-valuenow="80" aria-valuemin="0" aria-valuemax="100"></div>
+              </div>
+            </div>
+
+            <div class="progress">
+              <span class="skill">CSS <i class="val">70%</i></span>
+              <div class="progress-bar-wrap">
+                <div class="progress-bar" role="progressbar" aria-valuenow="70" aria-valuemin="0" aria-valuemax="100"></div>
+              </div>
+            </div>
+
+            <div class="progress">
+              <span class="skill">Javascript <i class="val">90%</i></span>
+              <div class="progress-bar-wrap">
+                <div class="progress-bar" role="progressbar" aria-valuenow="55" aria-valuemin="0" aria-valuemax="100"></div>
+              </div>
+            </div>
+
+          </div>
+
+        </div>
+
+      </div>
+    </section><!-- End Skills Section -->
     <!-- ======= Testimonials Section ======= -->
 <!--     <section id="testimonials" class="testimonials section-bg">
       <div class="container">
